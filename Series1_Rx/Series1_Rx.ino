ca432a38e9135eae6bf89ea113e3b6eaa5a2529d
srike27 <srike27@gmail.com>
2018-01-28 19:20:56 +0530

    rx done

--- Series1_Rx/Series1_Rx.ino
+++ Series1_Rx/Series1_Rx.ino
@@ -27,24 +27,24 @@ Error led is flashed if an unexpected packet is received
 
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
-// create reusable response objects for responses we expect to handle 
+// create reusable response objects for responses we expect to handle
 Rx16Response rx16 = Rx16Response();
 Rx64Response rx64 = Rx64Response();
 
 int statusLed = 11;
 int errorLed = 12;
 int dataLed = 10;
-
+uint8_t ch[50];
 uint8_t option = 0;
 uint8_t data = 0;
 
 void flashLed(int pin, int times, int wait) {
-    
+
     for (int i = 0; i < times; i++) {
       digitalWrite(pin, HIGH);
       delay(wait);
       digitalWrite(pin, LOW);
-      
+
       if (i + 1 < times) {
         delay(wait);
       }
@@ -55,47 +55,41 @@ void setup() {
   pinMode(statusLed, OUTPUT);
   pinMode(errorLed, OUTPUT);
   pinMode(dataLed,  OUTPUT);
-  
+
   // start serial
-  Serial.begin(9600);
+  Serial.begin(115200);
   xbee.setSerial(Serial);
-  
+
   flashLed(statusLed, 3, 50);
 }
 
 // continuously reads packets, looking for RX16 or RX64
 void loop() {
-    
+
     xbee.readPacket();
-    
+
     if (xbee.getResponse().isAvailable()) {
       // got something
-      
+
       if (xbee.getResponse().getApiId() == RX_16_RESPONSE || xbee.getResponse().getApiId() == RX_64_RESPONSE) {
         // got a rx packet
-        
-        if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
-                xbee.getResponse().getRx16Response(rx16);
-        	option = rx16.getOption();
-        	data = rx16.getData(0);
-        } else {
-                xbee.getResponse().getRx64Response(rx64);
-        	option = rx64.getOption();
-        	data = rx64.getData(0);
-        }
-        
-        // TODO check option, rssi bytes    
+
+         xbee.getResponse().getRx16Response(rx16);
+         int n=0;
+         for (int i = 0; i <  rx16.getDataLength(); i++) {
+              ch[i]=rx16.getData(i);
+              n++;
+            }
+                    // TODO check option, rssi bytes
         flashLed(statusLed, 1, 10);
-        
+
         // set dataLed PWM to value of the first byte in the data
-        analogWrite(dataLed, data);
-      } else {
+        analogWrite(dataLed, ch[0]);
+           }
+        }
+         else {
       	// not something we were expecting
-        flashLed(errorLed, 1, 25);    
+        flashLed(errorLed, 1, 25);
       }
-    } else if (xbee.getResponse().isError()) {
-      //nss.print("Error reading packet.  Error code: ");  
-      //nss.println(xbee.getResponse().getErrorCode());
-      // or flash error led
-    } 
-}
+      Serial.println(ch[0]);
+}Series1
